5ad8421a6f1b650f8ebf1c2b0c08e8f0c2093fae
Annie <annie22803834j@gmail.com>
2025-04-27 14:22:16 +0800

    1.使用EF Core Power Tools產生Models 2.設定DB連線字串 3.建立OrdersAPI

--- NorthwindAPI/Program.cs
+++ NorthwindAPI/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using NorthwindAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindDatabase")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
